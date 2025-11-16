9cafb8991238f98100f90e374904cb12f04af40f
SAI <2300033275@kluniversity.in>
2025-11-16 19:15:30 +0530

    updated

--- STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
+++ STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
@@ -12,7 +12,7 @@ import com.klef.dev.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/studentapi/")
+@RequestMapping("/studentapi")
 @CrossOrigin(origins = "*")
 public class StudentController {
 
